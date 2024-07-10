7d0e3cceb064f7cff10726932b4f5df483dce547
Veerapandi S <veerapandi_s@yahoo.com>
2024-07-10 16:13:19 +0530

    nav bar change

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -36,12 +36,19 @@ export function Navbar() {
 
     return account?.address ? (
       <>
-        <p className="bg-text px-4 py-1 flex justify-center items-center rounded-full font-medium cursor-pointer" onClick={() => setShowPopup(true)}>
+        <p className="bg-text flex justify-center items-center rounded-full font-medium cursor-pointer" style={{
+          fontSize : '12px',
+          padding : '4px'
+        }} onClick={() => setShowPopup(true)}>
           <span className="text-gradient">
             {loading ? "Loading..." : `${Number(balance).toFixed(2)} ${TOKEN_SYMBOL}`}
           </span>
         </p>
-        <p className="bg-text px-4 py-1 flex justify-center items-center rounded-full font-medium cursor-pointer" onClick={() => setShowPopup(true)}>
+        <p className="bg-text flex justify-center items-center rounded-full font-medium cursor-pointer" 
+        style={{
+          fontSize : '12px',
+          padding : '4px'
+        }} onClick={() => setShowPopup(true)}>
           <span className="text-gradient">
             {isNativeBalanceFetching ? "Loading..." : `${Number(nativeBalance).toFixed(2)} CHZ`}
           </span>
@@ -58,7 +65,7 @@ export function Navbar() {
       {/* {showAlert && <UserAlertPopup onClose={handleClosePopup} />} */}
       <a href="/events" className="cursor-pointer">
         <div className="text-xl font-semibold text-text tracking-wide text-base" style={{
-          width : '80px'
+          width : '150px'
         }}>
           <img src="/WakandaLogo/WakandaTransparentLight.png" alt="WakandaBets" className="transition hover:scale-105" />
         </div>

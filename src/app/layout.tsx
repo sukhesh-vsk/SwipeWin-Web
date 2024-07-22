ec2b478083634160f7d16c7c399e4f92edee83ac
Veerapandi S <veerapandi_s@yahoo.com>
2024-07-22 22:33:01 +0530

    g-tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,32 @@ export default function Layout(props: { children: React.ReactNode }) {
 
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-59LQD73Z');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className="bg-bg text-text font-inter flex flex-col">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-59LQD73Z"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <div className="mobile-wrapper">
           <div className="mobile-content no-scrollbar">
             <ServiceWorkerRegister />

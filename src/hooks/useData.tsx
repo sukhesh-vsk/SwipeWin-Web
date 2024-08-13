46c98a7fa3668e65a4caa3cfac06d2231ddd21bb
sukhesh-vsk <sukhesh.vsk2005@gmail.com>
2024-08-13 21:53:24 +0530

    add filter by country name

--- src/hooks/useData.tsx
+++ src/hooks/useData.tsx
@@ -74,6 +74,7 @@ const useData = (
       game.title?.toLowerCase().includes(searchTerm.toLowerCase()) ||
       game.sport.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
       game.league.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      game.league.country.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
       game.participants.some((participant) =>
         participant.name.toLowerCase().includes(searchTerm.toLowerCase())
       );
